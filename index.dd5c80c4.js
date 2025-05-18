1d6ab0b4853f7d4e807ecfc71d318c37ec838019
Nazar Bakay <bakayn484@gmail.com>
2025-05-18 20:40:09 +0300

    make build

---
+++ index.dd5c80c4.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table"),r=!0,l=!1,t=void 0;try{for(var n,o=e.rows[Symbol.iterator]();!(r=(n=o.next()).done);r=!0){var a=n.value,i=a.cells;if(!(1>=i.length)){var c=i[1].cloneNode(!0);i.length>=1?a.insertBefore(c,i[i.length-1]):a.appendChild(c)}}}catch(e){l=!0,t=e}finally{try{r||null==o.return||o.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.dd5c80c4.js.map
