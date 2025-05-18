acd2d13d4686e280324a5e7a738c8dafc6d872b1
Nazar Bakay <bakayn484@gmail.com>
2025-05-18 20:06:34 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write your code here
+
+const table = document.querySelector('table');
+const indexCopyColumn = 1;
+
+for (const row of table.rows) {
+  const cells = row.cells;
+  const ourCell = cells[indexCopyColumn];
+
+  const newCell = ourCell.cloneNode(true);
+
+  const indexPasteColumn = cells.length - 1;
+
+  row.insertBefore(newCell, cells[indexPasteColumn]);
+}
